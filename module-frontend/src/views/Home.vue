fcc2184461d45efa6366649328ea9d2eb3565f6b
tsirera <tsirera@vectoritcgroup.com>
2019-09-10 11:25:20 +0200

    soporte temas vuetify

--- module-frontend/src/views/Home.vue
+++ module-frontend/src/views/Home.vue
@@ -1,18 +1,46 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <app-helloWorld />
-    <hr />
-    <div class="flags">
-      <button
-        v-for="(language, index) in languages"
-        :key="index"
-        @click="updateLanguage(language.id)"
-      >
-        <app-country-flag :country="language.flag" />
-      </button>
-    </div>
-  </div>
+  <v-container fluid>
+      
+      <v-row>
+        <v-col>
+          <img alt="Vue logo" src="../assets/logo.png" />
+        </v-col>
+      </v-row>
+      
+      <v-row>
+        <v-col>
+          <app-helloWorld />
+        </v-col>
+      </v-row>
+      
+      <v-row>
+        <v-col>
+          <div class="flags">
+            <button
+              v-for="(language, index) in languages"
+              :key="index"
+              @click="updateLanguage(language.id)"
+            >
+              <app-country-flag :country="language.flag" />
+            </button>
+          </div>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col>
+          <v-combobox
+            v-model="selectedTheme"
+            :items="themes"
+            label="Select a theme"
+          ></v-combobox>
+        </v-col>
+        <v-col>
+          <v-switch v-model="dark" label="Dark"></v-switch>
+        </v-col>
+      </v-row>
+      
+  </v-container>
 </template>
 
 <script>
@@ -28,6 +56,9 @@ export default {
   },
   data() {
     return {
+      dark : false,
+      selectedTheme : null,
+      themes : Object.keys(window.t_manager.Themes),
       languages: [
         { flag: "es", id: "es" },
         { flag: "gb", id: "en" }
@@ -41,6 +72,16 @@ export default {
   },
   mounted() {
     this.$store.dispatch("fooModule/initFooData");
+  },
+  watch: {
+   selectedTheme(val){
+
+    this.$vuetify.theme.themes.light = window.t_manager.Themes[val].light;
+    this.$vuetify.theme.themes.dark = window.t_manager.Themes[val].dark;
+
+    this.$vuetify.theme.dark = this.dark;
+
+   } 
   }
 };
 </script>
