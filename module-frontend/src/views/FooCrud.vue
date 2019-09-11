4a3f378489d08df8f69edb444c77d0576f6ff8d5
tsirera <tsirera@vectoritcgroup.com>
2019-09-11 13:08:48 +0200

    vuetify en foocrud

--- module-frontend/src/views/FooCrud.vue
+++ module-frontend/src/views/FooCrud.vue
@@ -1,32 +1,65 @@
 <template>
-  <div class="foo">
-    <h1>{{ $t("fooCrud.welcome") }}</h1>
-    <p>
-      {{ $t("fooCrud.description") }}
-    </p>
-    <div>
-      <button type="button" @click="getException()">
-        {{ $t("fooCrud.exception") }}
-      </button>
-    </div>
-    <div>
-      {{ $t("fooCrud.total") }}-{{ nFoos }}
-      <ul>
-        <li v-for="(foo, index) in foos" :key="index">
-          <label>{{ foo.name }}-{{ foo.value }}</label>
-          <button type="button" @click="removeFoo(foo.value)">
-            {{ $t("fooCrud.remove") }}
-          </button>
-        </li>
-      </ul>
+  <v-container fluid>
+      <v-layout text-center wrap>
+        <v-flex xs12>
+          <h1>{{ $t("fooCrud.welcome") }}</h1>
+        </v-flex>
+      </v-layout>
 
-      <div>
-        <button type="button" @click="cretateFoo()">
-          {{ $t("fooCrud.create") }}
-        </button>
-      </div>
-    </div>
-  </div>
+      <v-layout text-center wrap>
+        <v-flex xs12>
+          <v-subheader>{{ $t("fooCrud.description") }}</v-subheader>
+        </v-flex>
+      </v-layout>
+
+      <v-layout text-center wrap>
+        <v-flex xs12>
+          <v-btn 
+           text 
+           @click="getException()"
+          >
+            {{ $t("fooCrud.exception") }}
+          </v-btn >
+        </v-flex>
+      </v-layout>
+
+      <v-layout text-center wrap>
+        <v-flex xs12>
+          <v-subheader>{{ $t("fooCrud.total") }}-{{ nFoos }}</v-subheader>
+          <v-list-item
+            v-for="(foo, index) in foos"
+            :key="index"
+          >
+            <v-list-item-content>
+              <v-list-item-title>
+                {{ foo.name }}-{{ foo.value }}
+              </v-list-item-title>
+
+              <v-btn 
+               text
+               @click="removeFoo(foo.value)"
+              >
+                {{ $t("fooCrud.remove") }}
+              </v-btn>
+
+            </v-list-item-content>
+          </v-list-item>
+          
+        </v-flex>
+      </v-layout>
+
+      <v-layout text-center wrap>
+        <v-flex xs12>
+          <v-btn 
+            @click="cretateFoo()"
+            text
+          >
+              {{ $t("fooCrud.create") }}
+          </v-btn>
+        </v-flex>
+      </v-layout>
+
+  </v-container>
 </template>
 
 <script>
