af367efe5ca5e8da816b3efa3c61479ac41b1d99
tsirera <tsirera@vectoritcgroup.com>
2019-10-07 12:50:12 +0200

    botones de color segun estilo seleccionado

--- module-frontend/src/views/Home.vue
+++ module-frontend/src/views/Home.vue
@@ -18,36 +18,33 @@
         </div>
       </v-col>
     </v-row>
-    <v-row>
-      <v-row>
-        <v-col
-          sm="12"
-          md="4"
-          class="text-center"
+    <v-row justify="center">
+      <v-col sm="12" md="1">
+        <v-combobox
+          v-model="selectedTheme"
+          :items="themes"
+          label="Select a theme"
         >
-          <v-combobox
-            v-model="selectedTheme"
-            :items="themes"
-            label="Select a theme"
-          >
-          </v-combobox>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col
-          sm="12"
-          md="4"
-          class="text-center"
-        >
-          <v-switch v-model="dark" label="Dark"> </v-switch>
-        </v-col>
-      </v-row>
+        </v-combobox>
+      </v-col>
+      <v-col sm="12" md="1">
+        <v-switch v-model="dark" label="Dark"> </v-switch>
+      </v-col>
+    </v-row>
+    <v-row justify="center">
+      <v-btn
+        v-for="(color, index) in colors"
+        :key="index"
+        :color="color"
+      >
+        {{color}}
+      </v-btn>
     </v-row>
   </v-container>
 </template>
 
 <script>
-import HelloWorld from "@/components/HelloWorld.vue"; 
+import HelloWorld from "@/components/HelloWorld.vue";
 import CountryFlag from "vue-country-flag";
 
 export default {
@@ -61,16 +58,18 @@ export default {
       dark: false,
       selectedTheme: null,
       themes: Object.keys(window.t_manager.Themes),
-      languages: [{ flag: "es", id: "es" }, { flag: "gb", id: "en" }]
+      languages: [{ flag: "es", id: "es" }, { flag: "gb", id: "en" }],
+      colors: []
     };
   },
   methods: {
     updateLanguage(id) {
       this.$store.dispatch("updateLanguage", id);
+    },
+    updateColors(){
+      const theme = this.$vuetify.theme.dark ? this.$vuetify.theme.themes.dark : this.$vuetify.theme.themes.light;
+      this.colors = Object.keys(theme);
     }
-  },
-  mounted() {
-    this.$store.dispatch("fooModule/initFooData");
   },
   watch: {
     selectedTheme(val) {
@@ -78,10 +77,16 @@ export default {
       this.$vuetify.theme.themes.dark = window.t_manager.Themes[val].dark;
 
       this.$vuetify.theme.dark = this.dark;
+      this.updateColors();
     },
     dark(val) {
       this.$vuetify.theme.dark = val;
+      this.updateColors();
     }
+  },
+  mounted() {
+    this.updateColors();
+    this.$store.dispatch("fooModule/initFooData");
   }
 };
 </script>
