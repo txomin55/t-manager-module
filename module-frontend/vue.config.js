96335365f8af0d1bd704e1bdf016ebeddf1ad396
tsirera <tsirera@vectoritcgroup.com>
2019-10-11 08:23:30 +0200

    readme

--- module-frontend/vue.config.js
+++ module-frontend/vue.config.js
@@ -4,6 +4,9 @@ module.exports = {
   // to our Spring Boot backend (localhost:8002) using http-proxy-middleware
   // see https://cli.vuejs.org/config/#devserver-proxy
   devServer: {
+    watchOptions: {
+      poll: true
+    },
     proxy: {
       "/": {
         target: "http://localhost:8002", // this configuration needs to correspond to the Spring Boot backends' application.properties server.port
