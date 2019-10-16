74413fc576bec825ec874e9bbbac0a6f1b9151d7
tsirera <tsirera@vectoritcgroup.com>
2019-10-16 12:16:50 +0200

    test de auth

--- module-web/src/test/java/com/tmanager/module/web/TManagerModuleWebTests.java
+++ module-web/src/test/java/com/tmanager/module/web/TManagerModuleWebTests.java
@@ -1,15 +1,79 @@
 package com.tmanager.module.web;
 
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
-@RunWith(SpringRunner.class)
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+@RunWith(MockitoJUnitRunner.class)
 @SpringBootTest
 public class TManagerModuleWebTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private JwtTokenStore tokenStore;
+
+	@Value("${module.oauth.clientId}")
+	private String clientId;
+
+	@Value("${module.oauth.clientSecret}")
+	private String clientSecret;
+
+	@Value("${module.oauth.server.path}")
+	private String serverPath;
+
+	@Value("${module.oauth.server.port}")
+	private String serverPort;
+
+	@Value("${module.oauth.server.address}")
+	private String serverAddress;
+
+	@Value("${module.oauth.clientRedirectUrl}")
+	private String clientRedirectUrl;
+	
+    @Test
+    public void whenTokenDontContainIssuer() {
+        final String tokenValue = obtainAccessToken();
+        final OAuth2Authentication auth = tokenStore.readAuthentication(tokenValue);
+        System.out.println(tokenValue);
+        System.out.println(auth);
+        assertTrue(auth.isAuthenticated());
+        System.out.println(auth.getDetails());
+
+        Map<String, Object> details = (Map<String, Object>) auth.getDetails();
+        assertTrue(details.containsKey("organization"));
+        System.out.println(details.get("organization"));
+    }
+
+    private String obtainAccessToken() {
+    	
+    	final Map<String, String> params = new HashMap<String, String>();
+    	String AUTH_SERVER = serverAddress + ":" + serverPort + "/" + serverPath + "/oauth";
+    	
+        params.put("grant_type", "password");
+        params.put("client_id", clientId);
+        params.put("username", "user-open-source");
+        params.put("password", "password-open-source");
+        final Response response = RestAssured.given()
+        		.auth()
+        		.preemptive()
+        		.basic(clientId, clientSecret)
+        		.and()
+        		.with()
+        		.params(params)
+        		.when()
+        		.post(AUTH_SERVER + "/token");
+        return response.jsonPath().getString("access_token");
+    }
 }
