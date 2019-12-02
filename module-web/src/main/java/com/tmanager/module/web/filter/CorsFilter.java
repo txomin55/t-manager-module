7e1efbb75393c632a655b7935b154d0724fcd7d2
txomin55 <txomin.sirera@gmail.com>
2019-12-02 22:23:59 +0100

    orden filtro cors

--- module-web/src/main/java/com/tmanager/module/web/filter/CorsFilter.java
+++ module-web/src/main/java/com/tmanager/module/web/filter/CorsFilter.java
@@ -10,9 +10,12 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
 
 	@Override
