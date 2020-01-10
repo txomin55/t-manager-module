32ec041a68807946591399dae1276a1fb8d9a68e
tsirera <tsirera@vectoritcgroup.com>
2020-01-10 11:29:05 +0100

    añadir restriccion de permisos

--- module-loader/src/main/java/com/tmanager/module/configuration/application/foo/FooApplicationConfiguration.java
+++ module-loader/src/main/java/com/tmanager/module/configuration/application/foo/FooApplicationConfiguration.java
@@ -33,8 +33,8 @@ public class FooApplicationConfiguration {
     }
 
     @Bean
-    public DeleteFooService deleteFooService(DeleteFooPersistancePort deleteFooPersistancePort) {
-        return new DeleteFooServiceAdapter(deleteFooPersistancePort);
+    public DeleteFooService deleteFooService(DeleteFooPersistancePort deleteFooPersistancePort, GetFooPersistancePort getFooPersistancePort) {
+        return new DeleteFooServiceAdapter(deleteFooPersistancePort, getFooPersistancePort);
     }
     
     @Bean
