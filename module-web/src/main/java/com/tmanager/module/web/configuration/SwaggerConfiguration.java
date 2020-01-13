04145f8d73b600dca90046425111ba5a11f9e26e
txomin55 <txomin.sirera@gmail.com>
2020-01-13 21:20:29 +0100

    swagger + oath2

--- module-web/src/main/java/com/tmanager/module/web/configuration/SwaggerConfiguration.java
+++ module-web/src/main/java/com/tmanager/module/web/configuration/SwaggerConfiguration.java
@@ -50,7 +50,7 @@ public class SwaggerConfiguration {
 	public Docket api() {
 	    return new Docket(DocumentationType.SWAGGER_2).select()
 	        .apis(RequestHandlerSelectors.any())
-	        .paths(PathSelectors.any())
+	        .paths(PathSelectors.regex("\\/api\\/.*"))
 	        .build()
 	        .securitySchemes(Arrays.asList(securityScheme()))
 	        .securityContexts(Arrays.asList(securityContext()));
